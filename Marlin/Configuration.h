cbf501d8837bd0c9104f644be00576c03fb8dbf3
Robert McIntyre <rjmcinty@hotmail.com>
2021-01-11 21:17:05 -0800

    Disable Linear Advance

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -497,26 +497,7 @@
   #define PID_FUNCTIONAL_RANGE 10 // If the temperature difference between the target temperature and the actual temperature
                                   // is more than PID_FUNCTIONAL_RANGE then the PID will be shut off and the heater will be set to min/max.
 
-  // If you are using a pre-configured hotend then you can use one of the value sets by uncommenting it
-  // Creality Ender-3
-  #define DEFAULT_Kp 21.73
-  #define DEFAULT_Ki 1.54
-  #define DEFAULT_Kd 76.55
 
-  // Ultimaker
-  //#define DEFAULT_Kp 22.2
-  //#define DEFAULT_Ki 1.08
-  //#define DEFAULT_Kd 114
-
-  // MakerGear
-  //#define DEFAULT_Kp 7.0
-  //#define DEFAULT_Ki 0.1
-  //#define DEFAULT_Kd 12
-
-  // Mendel Parts V9 on 12V
-  //#define DEFAULT_Kp 63.0
-  //#define DEFAULT_Ki 2.25
-  //#define DEFAULT_Kd 440
 
   #if ENABLED(PID_PARAMS_PER_HOTEND)
     // Specify between 1 and HOTENDS values per array.
@@ -525,9 +506,26 @@
     #define DEFAULT_Ki_LIST {   1.08,   1.08 }
     #define DEFAULT_Kd_LIST { 114.00, 114.00 }
   #else
-    #define DEFAULT_Kp  22.20
-    #define DEFAULT_Ki   1.08
-    #define DEFAULT_Kd 114.00
+    // If you are using a pre-configured hotend then you can use one of the value sets by uncommenting it
+    // Creality Ender-3
+    #define DEFAULT_Kp 21.73
+    #define DEFAULT_Ki 1.54
+    #define DEFAULT_Kd 76.55
+
+    // Ultimaker
+    //#define DEFAULT_Kp 22.2
+    //#define DEFAULT_Ki 1.08
+    //#define DEFAULT_Kd 114
+
+    // MakerGear
+    //#define DEFAULT_Kp 7.0
+    //#define DEFAULT_Ki 0.1
+    //#define DEFAULT_Kd 12
+
+    // Mendel Parts V9 on 12V
+    //#define DEFAULT_Kp 63.0
+    //#define DEFAULT_Ki 2.25
+    //#define DEFAULT_Kd 440
   #endif
 #endif // PIDTEMP
 
